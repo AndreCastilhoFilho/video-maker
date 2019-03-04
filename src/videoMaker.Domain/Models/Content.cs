3fb3470f44a2be681fd72b276adc81e929320407
André Castilho <andre.filho@positivo.com.br>
2019-03-04 16:57:32 -0300

    first commit

---
+++ src/videoMaker.Domain/Models/Content.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace videoMaker.Domain.Models
+{
+    public abstract class Content
+    {
+
+        public string SearchTerm { get; set; }
+        public string Prefix { get; set; }
+        public string SourceContentOriginal { get; set; }
+        public string SourceContentSanitized { get; set; }
+        public IList<Sentence> Sentences { get; set; }
+
+        public class Sentence
+        {
+            public string Text { get; set; }
+            public string[] Keywords { get; set; }
+            public string[] Images { get; set; }
+
+        }
+    }
+}
