c607c1d7fe2cd0b6d4a85bf6ea9ad835fad03435
André Castilho <andre.filho@positivo.com.br>
2019-03-15 13:59:14 -0300

    limpar conteúdo

--- src/videoMaker.Domain/Robots/Text.cs
+++ src/videoMaker.Domain/Robots/Text.cs
@@ -1,5 +1,6 @@
 ﻿using Algorithmia;
 using System;
+using System.Text.RegularExpressions;
 using videoMaker.Domain.Models;
 
 namespace videoMaker.Domain.Robots
@@ -18,8 +19,8 @@ namespace videoMaker.Domain.Robots
         {
 
             FetchContentFromWikipedia();
-            // SanitizeContent();
-            //  BrakContentIntoSentences();
+            SanitizeContent();
+            //BrakContentIntoSentences();
 
             Console.WriteLine($"Recebi com sucesso o content:{ Content.ToString() }");
 
@@ -33,12 +34,38 @@ namespace videoMaker.Domain.Robots
             var wikipidiaResponse = algorithm.pipe<object>(Content.SearchTerm);
             var wikipidiaContent = wikipidiaResponse.result.ToString();
 
-            Console.WriteLine(wikipidiaContent);
+
+            Content.SourceContentOriginal = wikipidiaContent;
+
         }
 
         private void SanitizeContent()
         {
-            throw new NotImplementedException();
+            var withoutBlankLines = RemoveBlankLines(Content.SourceContentOriginal);
+            var withoutMarkDown = RemoveMarkdown(withoutBlankLines);
+            var withoutDateInParenteses = RemoveDateInParentheses(withoutMarkDown);
+
+            Content.SourceContentSanitized = withoutDateInParenteses;
+
+            Console.WriteLine(withoutDateInParenteses);
+
+        }
+
+        private string RemoveDateInParentheses(string text)
+        {
+            return Regex.Replace(text, @"/\((?:\([^()]*\)|[^()])*\)", string.Empty, RegexOptions.Multiline);
+
+        }
+
+        private string RemoveMarkdown(string text)
+        {
+            return Regex.Replace(text, @"^=*", string.Empty, RegexOptions.Multiline).Replace("===", "").Replace("==", "");
+        }
+
+        private string RemoveBlankLines(string text)
+        {
+            return Regex.Replace(text, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline)
+                .Replace("\\n", "\r\n");
         }
 
         private void BrakContentIntoSentences()
